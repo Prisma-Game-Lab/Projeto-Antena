c08ec61f677262c8a80b1686f29b9d86c7162f4e
Raphael Fleury <raphael.fleury@b2wdigital.com>
2020-10-18 16:03:46 -0300

    Pause menu no jogo

--- Time-Digital-2/Assets/Packages - Pipeline/Scripts/SceneController.cs
+++ Time-Digital-2/Assets/Packages - Pipeline/Scripts/SceneController.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using System;
 
 public class SceneController : MonoBehaviour
 {
 
+    public static bool gameIsPause = false;
+    public GameObject pauseMenuUI;
+
     void Start()
     {
-        DontDestroyOnLoad(this);
+        
     }
-   
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gameIsPause)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPause = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPause = false;
+    }
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);

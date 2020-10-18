0a269bfbe00434479f44f0a0ad17f1ca24a3eebd
Raphael Fleury <raphael.fleury@b2wdigital.com>
2020-10-18 07:46:49 -0300

    Criando cena do Main Menu

---
+++ Time-Digital-2/Assets/Packages - Pipeline/Scripts/SceneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+   
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+}

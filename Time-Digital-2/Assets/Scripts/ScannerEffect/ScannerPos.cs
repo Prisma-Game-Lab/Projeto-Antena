7ad83bb976054ae6eb149153ee855f17fe650fc2
rodrigomalhaesbuko <rodrigomalhaes@aluno.puc-rio.br>
2020-10-29 22:44:59 -0300

    Change project to HDRP

---
+++ Time-Digital-2/Assets/Scripts/ScannerEffect/ScannerPos.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+
+using UnityEngine.Rendering;
+
+using UnityEngine.Rendering.HighDefinition;
+
+using System;
+
+[Serializable, VolumeComponentMenu("Post-processing/Custom/ScannerPos")]
+
+public sealed class ScannerPos : CustomPostProcessVolumeComponent, IPostProcessComponent
+
+{
+
+    [Tooltip("Controls the intensity of the effect.")]
+
+    public ClampedFloatParameter intensity = new ClampedFloatParameter(0f, 0f, 1f);
+
+    Material m_Material;
+
+    public bool IsActive() => m_Material != null && intensity.value > 0f;
+
+    public override CustomPostProcessInjectionPoint injectionPoint => CustomPostProcessInjectionPoint.AfterPostProcess;
+
+    public override void Setup()
+
+    {
+
+        if (Shader.Find("Hidden/Shader/ScannerPos") != null)
+
+            m_Material = new Material(Shader.Find("Hidden/Shader/ScannerPos"));
+
+    }
+
+    public override void Render(CommandBuffer cmd, HDCamera camera, RTHandle source, RTHandle destination)
+
+    {
+
+        if (m_Material == null)
+
+            return;
+
+        m_Material.SetFloat("_Intensity", intensity.value);
+
+        m_Material.SetTexture("_InputTexture", source);
+        m_Material.SetVector("_WorldSpaceScannerPos", ScannerEffect.se.ScannerOrigin.position);
+        m_Material.SetFloat("_ScanDistance", ScannerEffect.se.ScanDistance);
+        RaycastCornerBlit(source, destination, m_Material);
+
+        HDUtils.DrawFullScreen(cmd, m_Material, destination);
+
+    }
+
+    public override void Cleanup() => CoreUtils.Destroy(m_Material);
+
+    void RaycastCornerBlit(RenderTexture source, RenderTexture dest, Material mat)
+    {
+        // Compute Frustum Corners
+        float camFar = ScannerEffect.se._camera.farClipPlane;
+        float camFov = ScannerEffect.se._camera.fieldOfView;
+        float camAspect = ScannerEffect.se._camera.aspect;
+
+        float fovWHalf = camFov * 0.5f;
+
+        Vector3 toRight = ScannerEffect.se._camera.transform.right * Mathf.Tan(fovWHalf * Mathf.Deg2Rad) * camAspect;
+        Vector3 toTop = ScannerEffect.se._camera.transform.up * Mathf.Tan(fovWHalf * Mathf.Deg2Rad);
+
+        Vector3 topLeft = (ScannerEffect.se._camera.transform.forward - toRight + toTop);
+        float camScale = topLeft.magnitude * camFar;
+
+        topLeft.Normalize();
+        topLeft *= camScale;
+
+        Vector3 topRight = (ScannerEffect.se._camera.transform.forward + toRight + toTop);
+        topRight.Normalize();
+        topRight *= camScale;
+
+        Vector3 bottomRight = (ScannerEffect.se._camera.transform.forward + toRight - toTop);
+        bottomRight.Normalize();
+        bottomRight *= camScale;
+
+        Vector3 bottomLeft = (ScannerEffect.se._camera.transform.forward - toRight - toTop);
+        bottomLeft.Normalize();
+        bottomLeft *= camScale;
+
+        // Custom Blit, encoding Frustum Corners as additional Texture Coordinates
+        RenderTexture.active = dest;
+
+        mat.SetTexture("_MainTex", source);
+
+        GL.PushMatrix();
+        GL.LoadOrtho();
+
+        mat.SetPass(0);
+
+        GL.Begin(GL.QUADS);
+
+        GL.MultiTexCoord2(0, 0.0f, 0.0f);
+        GL.MultiTexCoord(1, bottomLeft);
+        GL.Vertex3(0.0f, 0.0f, 0.0f);
+
+        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.MultiTexCoord(1, bottomRight);
+        GL.Vertex3(1.0f, 0.0f, 0.0f);
+
+        GL.MultiTexCoord2(0, 1.0f, 1.0f);
+        GL.MultiTexCoord(1, topRight);
+        GL.Vertex3(1.0f, 1.0f, 0.0f);
+
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
+        GL.MultiTexCoord(1, topLeft);
+        GL.Vertex3(0.0f, 1.0f, 0.0f);
+
+        GL.End();
+        GL.PopMatrix();
+    }
+
+}

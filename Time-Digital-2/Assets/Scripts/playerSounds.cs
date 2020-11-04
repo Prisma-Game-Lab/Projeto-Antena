eafda7490a842f8ed7be8a11499fbc4c6396513d
TheoCaldas <62903584+TheoCaldas@users.noreply.github.com>
2020-11-04 19:43:24 -0300

    Player andando Sound no Prefab

---
+++ Time-Digital-2/Assets/Scripts/playerSounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerSounds : MonoBehaviour
+{
+    private AudioSource andando;
+    private playerMovement pm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pm = this.GetComponent<playerMovement>();
+        andando = this.GetComponentInChildren<AudioSource>();
+        andando.loop = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!andando.isPlaying && pm.isMoving)
+        {
+            andando.Play();
+        }
+        else if (andando.isPlaying && !pm.isMoving)
+        {
+            andando.Pause();
+        }
+    }
+}

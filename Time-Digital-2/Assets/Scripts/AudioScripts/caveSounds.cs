3a00773a3edf3b12b23aa0de4879ca854e2c4713
TheoCaldas <62903584+TheoCaldas@users.noreply.github.com>
2020-11-05 00:26:09 -0300

    cave sounds 2

---
+++ Time-Digital-2/Assets/Scripts/AudioScripts/caveSounds.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class caveSounds : MonoBehaviour
+{
+    public AudioSource cave1;
+    public AudioSource cave3;
+
+    private float timer = 0.0f;
+    private float timeLimit = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cave1.loop = false;
+        cave3.loop = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= timeLimit)
+        {
+            timeLimit = Random.Range(40.0f, 50.0f);
+            timer = 0.0f;
+
+            int i = (int)Random.Range(0, 2);
+            if (i == 0)
+            {
+                cave1.Play();
+            }
+            else
+            {
+                cave3.Play();
+            }
+        }
+        else
+        {
+            timer += Time.deltaTime;
+            //Debug.Log(timer);
+        }
+    }
+}

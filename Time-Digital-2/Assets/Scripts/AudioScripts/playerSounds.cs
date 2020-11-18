1d1728bdece1ed58e661c87a343c912479f3062d
TheoCaldas <62903584+TheoCaldas@users.noreply.github.com>
2020-11-18 13:12:13 -0300

    som sonar + cooldown

--- Time-Digital-2/Assets/Scripts/AudioScripts/playerSounds.cs
+++ Time-Digital-2/Assets/Scripts/AudioScripts/playerSounds.cs
@@ -4,27 +4,64 @@ using UnityEngine;
 
 public class playerSounds : MonoBehaviour
 {
-    private AudioSource andando;
+    public AudioSource andando;
+    public AudioSource morte;
+    public AudioSource cooldown;
+
+    public GameObject sonarList;
+    private AudioSource[] eco;
+
+    private bool sonarPlayed = false;
+
+    //ref outros scripts
     private playerMovement pm;
+    private ScannerGenerator sGen;
 
     // Start is called before the first frame update
     void Start()
     {
-        //pm = this.GetComponent<playerMovement>();
+        pm = this.GetComponent<playerMovement>();
+        sGen = this.GetComponent<ScannerGenerator>();
+
+        eco = sonarList.transform.GetComponentsInChildren<AudioSource>();
         //andando = this.GetComponentInChildren<AudioSource>();
-        //andando.loop = true;
+        andando.loop = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (!andando.isPlaying && pm.isMoving)
-        //{
-        //    andando.Play();
-        //}
-        //else if (andando.isPlaying && !pm.isMoving)
-        //{
-        //    andando.Pause();
-        //}
+        UpdateAndando();
+        UpdateSonar();
+    }
+
+    void UpdateAndando()
+    {
+        if (!andando.isPlaying && pm.isMoving)
+        {
+            andando.Play();
+        }
+        else if (andando.isPlaying && !pm.isMoving)
+        {
+            andando.Pause();
+        }
+    }
+
+    void UpdateSonar()
+    {
+        if (!sonarPlayed && !sGen.canUseSonar)
+        {
+            sonarPlayed = true;
+            //toca sonar
+            int i = (int)Random.Range(0, eco.Length);
+            AudioSource randEco = eco[i];
+            randEco.Play();
+        }
+        if (sonarPlayed && sGen.canUseSonar)
+        {
+            sonarPlayed = false;
+            //toca cooldown
+            cooldown.Play();
+        }
     }
 }

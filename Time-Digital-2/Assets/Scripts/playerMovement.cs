e7cac0f8a1852014504a42272c4c72e81b662b7b
vitorsalek <59521603+vitorsalek@users.noreply.github.com>
2020-11-19 20:22:11 -0300

    Revert "Esconderijo e camera primeira pessoa"
    This reverts commit 6c4809172533164558adf031a4d2eae21f149748.

--- Time-Digital-2/Assets/Scripts/playerMovement.cs
+++ Time-Digital-2/Assets/Scripts/playerMovement.cs
@@ -3,28 +3,21 @@
 
 public class playerMovement : MonoBehaviour
 {
-    public GameObject mainCam;
-    public GameObject thirdPersonCam;
-    public GameObject firstPersonCam;
-    public SceneController sceneController;
-
+    public Transform cam;
     public float movementForce;
     public float movementSpeed;
+    public Rigidbody rb;
+    public SceneController sceneController;
 
+    //Velocidade de rotação do player
+    public float turnSmoothTime=0.1f;
+    private float turnSmoothVelocity;
+    
     [HideInInspector]
     public bool isMoving;
-    [HideInInspector]
-    public bool isSafe;
 
     public static playerMovement current;
 
-    //Velocidade de rotação do player
-    private float turnSmoothTime=0.1f;
-    private float turnSmoothVelocity;
-    private Rigidbody playerRb;
-    private bool thirdPersonMode;
-
-    
     private void Awake()
     {
         current = this;
@@ -32,27 +25,17 @@ public class playerMovement : MonoBehaviour
 
     private void Start()
     {
-        playerRb = GetComponent<Rigidbody>();
         //Trava e deixa o cursor invisivel
-        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.lockState = CursorLockMode.Locked;+
         isMoving = false;
-        isSafe = false;
-        thirdPersonMode = true;
-
-        thirdPersonCam.SetActive(thirdPersonMode);
-        firstPersonCam.SetActive(!thirdPersonMode);
-
     }
 
     private void FixedUpdate()
     {
         Movement();
-        if (!thirdPersonCam.activeSelf)
-            transform.rotation = Quaternion.AngleAxis(mainCam.transform.rotation.eulerAngles.y * Time.fixedDeltaTime * 50, Vector3.up);
     }
 
-    private void Movement()
-    {
+    private void Movement(){
         isMoving = false;
         //Pega input horizontal e vertical
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -67,23 +50,22 @@ public class playerMovement : MonoBehaviour
             isMoving = true;
 
             //Calcula o angulo que o player precisa rotacionar, baseado na direção dos inputs
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCam.transform.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
 
             //Transição de rotação suave
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            if(thirdPersonCam.activeSelf)
-                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             //Calcula direção baseada no angulo de rotação para ser uma direção relativa a camera
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
             //Adiciona força no player
-            playerRb.AddForce(moveDir * Time.deltaTime * movementForce * 1000f);
+            rb.AddForce(moveDir * Time.deltaTime * movementForce * 1000f);
             //Se velocidade atual maior que velocidade maxima
-            if (playerRb.velocity.magnitude >= movementSpeed)
+            if (rb.velocity.magnitude >= movementSpeed)
             {
                 //Velocidade atual é igual a máxima
-                playerRb.velocity = playerRb.velocity.normalized * movementSpeed;
+                rb.velocity = rb.velocity.normalized * movementSpeed;
             }
         }
     }
@@ -94,27 +76,6 @@ public class playerMovement : MonoBehaviour
         {
             Debug.Log("Colisao");
             sceneController.LoadScene(sceneController.currentScene.name);
-        }
-
-    }
-    private void OnTriggerEnter(Collider collision)
-    {
-        if (collision.gameObject.CompareTag("SafeSpot"))
-        {
-            Debug.Log("Entrou esconderijo");
-            thirdPersonCam.SetActive(!thirdPersonMode);
-            firstPersonCam.SetActive(thirdPersonMode);
-            isSafe = true;
-        }
-    }
-    private void OnTriggerExit(Collider collision)
-    {
-        if (collision.gameObject.CompareTag("SafeSpot"))
-        {
-            Debug.Log("Saiu esconderijo");
-            thirdPersonCam.SetActive(thirdPersonMode);
-            firstPersonCam.SetActive(!thirdPersonMode);
-            isSafe = false;
         }
     }
 }

68f0751757da84b46cfec3c4a03c1d981eb8e81c
TheoCaldas <62903584+TheoCaldas@users.noreply.github.com>
2020-12-04 02:39:25 -0300

    alarme

---
+++ Time-Digital-2/Assets/Audio/AudioScripts/doorSounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorSounds : MonoBehaviour
+{
+    public AudioSource alarme;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playAlarme()
+    {
+        alarme.Play();
+        Debug.Log("ALARME");
+    }
+}

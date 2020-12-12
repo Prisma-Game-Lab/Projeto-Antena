152a3441a60f43af35e0f796b2f4b10ce2371ab0
rodrigomalhaesbuko <rodrigomalhaes@aluno.puc-rio.br>
2020-12-12 20:11:24 -0300

    save system half implemented

---
+++ Time-Digital-2/Assets/Scripts/SaveSystem/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+   public Vector3 position;
+   public int checkpointsCount;
+
+   public Player GetNewPlayer()
+   {
+        Player newPlayer = new Player
+        {
+            checkpointsCount = 0
+        };
+
+        return newPlayer;
+   }
+
+
+   public void SavePlayer(Player player)
+   {
+        SaveSystem.SaveGame(player);
+   }
+
+
+   public Player LoadPlayer()
+   {
+        PlayerInfo info = SaveSystem.LoadGame();
+        Player newPlayer = new Player
+        {
+            checkpointsCount = info.checkpointsCount,
+            position = new Vector3(info.position[0], info.position[1], info.position[2])
+        };
+        return newPlayer;
+    }
+}

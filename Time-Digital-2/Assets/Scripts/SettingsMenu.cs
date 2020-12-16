5432154ebb513efe011cb4c0654429b454f0d516
Raphael Fleury <raphael.fleury@b2wdigital.com>
2020-12-15 22:30:49 -0300

    salvando volume no settings menu

--- Time-Digital-2/Assets/Scripts/SettingsMenu.cs
+++ Time-Digital-2/Assets/Scripts/SettingsMenu.cs
@@ -3,19 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer musicAudio;
     public AudioMixer effectsAudio;
 
-   public void SetEffectsAudio (float volume)
+    public Slider sfxSlider;
+    public Slider musicSlider;
+ 
+
+    private void Start()
+    {
+        float sfxVolume;
+        float musicVolume;
+
+        sfxVolume = PlayerPrefs.GetFloat("sfxVolume");
+        musicVolume = PlayerPrefs.GetFloat("musicVolume");
+
+        if(sfxVolume != null)
+        {
+            effectsAudio.SetFloat("volume", sfxVolume);
+            sfxSlider.value = sfxVolume;
+        }
+
+        if (musicVolume != null)
+        {
+            musicAudio.SetFloat("volume", musicVolume);
+            musicSlider.value = musicVolume;
+        }
+    }
+
+    public void SetEffectsAudio (float volume)
     {
         effectsAudio.SetFloat("volume", volume);
+        PlayerPrefs.SetFloat("sfxVolume", volume);
+        PlayerPrefs.Save();
     }
 
     public void SetMusicAudio(float volume)
     {
         musicAudio.SetFloat("volume", volume);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+        PlayerPrefs.Save();
     }
 }

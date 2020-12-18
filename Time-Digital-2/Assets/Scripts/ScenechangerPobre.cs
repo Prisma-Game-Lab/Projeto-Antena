fecdd4b42beb690b6d645f7237b51ad82de37623
pedro <pferndd@gmail.com>
2020-12-18 12:33:38 -0300

    creditos + prisma video

---
+++ Time-Digital-2/Assets/Scripts/ScenechangerPobre.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenechangerPobre : MonoBehaviour
+{
+    public void LoadB(int sceneANumber)
+    {
+        SceneManager.LoadScene(sceneANumber);
+    }
+}
---
+++ Time-Digital-2/Assets/Scripts/videoController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class videoController : MonoBehaviour
+{
+    VideoPlayer video;
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.loopPointReached += CheckOver;
+    }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene(0);
+    }
+}

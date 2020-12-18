5d0696e4dd6addafacbeefcdeac603643c93af04
pedro <pferndd@gmail.com>
2020-12-17 21:42:55 -0300

    prop edit

--- Time-Digital-2/Assets/Scripts/Player, Key, Enemy/doorConfig.cs
+++ Time-Digital-2/Assets/Scripts/Player, Key, Enemy/doorConfig.cs
@@ -7,11 +7,27 @@ public class doorConfig : MonoBehaviour
     public int doorPassword;
     [HideInInspector]
     public bool openDoor = false;
+    private Animator anim;
+    public GameObject ledVerde, ledVermelho;
+    public Material verdeAceso, vermelhoApagado;
+    private Material[] rendererMaterials, rendererMaterials2;
+
+    private void Start()
+    {
+        anim = this.GetComponent<Animator>();
+        rendererMaterials = ledVermelho.GetComponent<Renderer>().materials;
+        rendererMaterials2 = ledVerde.GetComponent<Renderer>().materials;
+    }
     private void Update()
     {
         if (openDoor)
         {
-            gameObject.SetActive(false);
+            anim.SetTrigger("open");
+            rendererMaterials[1] = verdeAceso;
+            rendererMaterials2[1] = vermelhoApagado;
+            ledVerde.GetComponent<Renderer>().materials = rendererMaterials;
+            ledVermelho.GetComponent<Renderer>().materials = rendererMaterials2;
+            //gameObject.SetActive(false);
         }
     }
 }
